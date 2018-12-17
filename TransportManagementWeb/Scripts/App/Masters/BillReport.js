df45ece27c379243521e521130e7d52a946dd7b9
Sheikh Imran <sheik.imran@onsolve.com>
2018-12-17 20:49:02 +0530

    bill report worked

--- TransportManagementWeb/Scripts/App/Masters/BillReport.js
+++ TransportManagementWeb/Scripts/App/Masters/BillReport.js
@@ -16,8 +16,13 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             success: function (data) {
                 $.each(data, function (key, entry) {
-                    dropdown.append($('<option></option>').attr('value', entry.ClientId).text(entry.InvoiceNumber));
+                    dropdown.append($('<option></option>').attr('value', entry.Id).text(entry.InvoiceNumber));
                 });
+                var invoiceId = getUrlParameter('invoiceNo');
+                if (invoiceId !== null && typeof invoiceId !== 'undefined') {
+                    $('#InvoiceDropdown').val(invoiceId);
+                    //$('#InvoiceDropdown').change();
+                }
             },
             failure: function (response) {
                 alert(response);
@@ -31,26 +36,44 @@ $(document).ready(function () {
     $('#InvoiceDropdown').on('change', function (e) {
         var invoiceNo = this.value;
         var url = "/Masters/BillReport?invoiceNo=" + invoiceNo;
-        window.location.href = url; 
+        window.location.href = url;
         //FillBillDetail(valueSelected);
     });
-    //function FillBillDetail(invoiceId) {
-    //    $.ajax({
-    //        dataType: 'json',
-    //        type: 'POST',
-    //        url: '/Masters/GetBillDetailByInvoiceId',
-    //        data: '{invoiceId: "' + invoiceId + '" }',
-    //        contentType: "application/json; charset=utf-8",
-    //        success: function (data) {
-              
-    //        },
-    //        failure: function (response) {
-    //            alert(response);
-    //        },
-    //        error: function (response) {
-    //            alert(response.responseText);
-    //        }
-    //    });
-    //}
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+    $(document).on('click', '#btnPrint', function () {
+        var pageTitle = 'Bill Report',
+            stylesheet = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css',
+            win = window.open('', 'Print', 'width=1000,height=1000');
+        $('#header').removeClass('hidden');
+        $('.table-responsive').removeClass('table-responsive');
+        $('#invoiceRow').addClass('hidden');
+        var dd = $('.content').clone();
+        //dd.find('tbody').append($('.table:eq(1) tbody tr').clone());
+
+        win.document.write('<html><head><title>' + pageTitle + '</title>' +
+            '<link rel="stylesheet" href="' + stylesheet + '">' +
+            '</head><body><hr/>' + dd.html() + '</body></html > ');
+        win.document.close();
+        win.print();
+        //win.close();
+        $('#header').addClass('hidden');
+        $('table.table-bordered').addClass('table-responsive');
+        $('#invoiceRow').removeClass('hidden');
+        return false;
+    });
 });
 
